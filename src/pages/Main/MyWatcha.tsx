db907da662263162c42ae73a3da121c3ec819745
taemin moon <tmmoond8@gmail.com>
2022-03-19 15:32:01 +0900

    ✨ 나의 왓챠 페이지 추가

---
+++ src/pages/Main/MyWatcha.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+import Layout from "../../components/Layout";
+
+export default function MyWatcha() {
+  return (
+    <Layout.Page px="16px">
+      <Text fontSize={22} fontWeight={700}>
+        MyWatcha
+      </Text>
+    </Layout.Page>
+  );
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,6 +2,7 @@ import { Tabs, Tab } from "../../tab";
 import Home from "./Home";
 import Search from "./Search";
 import Rate from "./Rate";
+import MyWatcha from "./MyWatcha";
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Tab name="홈" component={Home} />
       <Tab name="검색" component={Search}></Tab>
       <Tab name="평가" component={Rate}></Tab>
+      <Tab name="나의 왓챠" component={MyWatcha}></Tab>
     </Tabs>
   );
 }
