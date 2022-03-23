b70e31f4997c3b4c8e731f897d5b02eacc792d56
taemin moon <tmmoond8@gmail.com>
2022-03-23 14:39:20 +0900

    ✨ 평가하기 탭 렌더링

--- src/pages/Main/Rate.tsx
+++ src/pages/Main/Rate.tsx
@@ -1,12 +1,87 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Center,
+  List,
+  ListItem,
+  Image,
+  Stack,
+  HStack,
+} from "@chakra-ui/react";
+import useSWR from "swr";
+import { IoIosStar, IoIosMore } from "react-icons/io";
 import Layout from "../../components/Layout";
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export default function Rate() {
+  const { data: moviesData } = useSWR<{
+    movies: {
+      code: string;
+      title: string;
+      year: number;
+      poster: {
+        medium: string;
+      };
+      nations: {
+        name: string;
+      }[];
+    }[];
+  }>("/api/getMovies.json", fetcher);
   return (
-    <Layout.Page px="16px">
-      <Text fontSize={22} fontWeight={700}>
-        Rate
-      </Text>
+    <Layout.Page>
+      <Center flexDir="column">
+        <Text fontWeight="800" fontSize="2xl">
+          0
+        </Text>
+        <Text color="#666" mt="4px">
+          보셨던 영화를 평가해주세요.
+        </Text>
+      </Center>
+      <List
+        position="relative"
+        mt="16px"
+        borderTop="1px solid #eee"
+        borderBottom="1px solid #eee"
+      >
+        {moviesData?.movies?.map((movie) => (
+          <ListItem display="flex" p="16px 24px" key={movie.code}>
+            <Image
+              src={movie.poster.medium}
+              w="20vw"
+              h="auto"
+              objectFit="cover"
+            />
+            <Stack ml="16px">
+              <Text fontSize="lg" fontWeight="600">
+                {movie.title}
+              </Text>
+              <Text mt="0 !important" fontSize="sm" color="#666">
+                {`${movie.year} ・ ${movie.nations[0].name}`}
+              </Text>
+              <HStack
+                mt="20px !important"
+                css={{
+                  svg: {
+                    marginLeft: "0 !important",
+                  },
+                }}
+              >
+                {Array.from({ length: 5 }).map((_, idx) => (
+                  <IoIosStar key={idx} size={44} color="#eee" />
+                ))}
+              </HStack>
+            </Stack>
+            <Box
+              position="absolute"
+              top="12px"
+              right="24px"
+              transform="rotate(90deg)"
+            >
+              <IoIosMore color="#999" size="20px" />
+            </Box>
+          </ListItem>
+        ))}
+      </List>
     </Layout.Page>
   );
 }
--- src/pages/Main/Search.tsx
+++ src/pages/Main/Search.tsx
@@ -1,16 +1,38 @@
-import { Text, Input, Button, Flex } from "@chakra-ui/react";
+import { Text, Input, Button, Flex, List, ListItem } from "@chakra-ui/react";
 import Layout from "../../components/Layout";
 
 export default function Search() {
+  const popularList = [
+    "벨파스트",
+    "뜨거운 피",
+    "드라이브 마이 카",
+    "코다",
+    "69세",
+  ];
   return (
     <Layout.Page px="16px">
-      <Text fontSize={22} fontWeight={700}>
-        Search
-      </Text>
       <Flex>
         <Input />
-        <Button>Search</Button>
+        <Button ml="16px" colorScheme="pink">
+          Search
+        </Button>
       </Flex>
+      <Text fontSize={22} fontWeight={700} my="12px">
+        인기 검색어
+      </Text>
+      <List>
+        {popularList.map((item) => (
+          <ListItem
+            key={item}
+            fontSize="16px"
+            fontWeight="600"
+            p="12px 0"
+            boxShadow="0 1px #eee"
+          >
+            {item}
+          </ListItem>
+        ))}
+      </List>
     </Layout.Page>
   );
 }
